4f9b11af41bb256778099f337c3e187e77e6dba1
lyc <u202113971@hust.edu.cn>
2024-01-05 19:02:44 +0800

    Update correct.js

--- js/correct.js
+++ js/correct.js
@@ -1,6 +1,6 @@
 
-// var number1Element = document.getElementById('number-1');
-// var number2Element = document.getElementById('number-2');
-// number1Element.innerHTML = '8';
-// number2Element.textContent = '8';
+var number1Element = document.getElementById('number-1');
+var number2Element = document.getElementById('number-2');
+number1Element.innerHTML = '8';
+number2Element.textContent = '8';
 
